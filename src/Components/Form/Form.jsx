ab57cb2158d57fe1108dc5131e0094641196bfb7
Ahsan Shiekh <ahsanshiekh58@gmail.com>
2021-03-16 19:54:06 +0500

    removed aos from Form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -3,13 +3,7 @@ import "./Form.scss";
 
 const Form = ({ children, onSubmit }) => {
   return (
-    <form
-      data-aos="fade-up"
-      data-aos-duration="300"
-      data-aos-delay="900"
-      className="Form"
-      onSubmit={onSubmit}
-    >
+    <form className="Form" onSubmit={onSubmit}>
       {children}
     </form>
   );
